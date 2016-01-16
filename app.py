02d784e32a459d6e8d1af9a1da2ee6021e498d8f
Brian Ho <ho_brian@me.com>
2016-01-16 11:03:28 -0500

    adjust server code
    creates geoJSON format output with switched lat lng positions. needs JS
    update.

--- app.py
+++ app.py
@@ -49,6 +49,10 @@ def daymaker(time):
 
 	return dayscore.days
 
+def cleanText(oldStr):
+	newStr = oldStr.replace("\xc2\xa0", "")
+	return newStr
+
 def event_stream():
     while True:
         result = q.get()
@@ -92,7 +96,7 @@ def getData():
 		sys.exit()
 
 	#FIRST QUERY TO DATABASE, FILTERING USERS AND LIMITING
-	query = 'SELECT * FROM USER WHERE CNY = 7 limit 2'
+	query = 'SELECT * FROM USER WHERE CNY = 7 limit 10'
 
 
 	records = client.command(query)
@@ -101,7 +105,7 @@ def getData():
 	numListings = len(records)
 	print 'received ' + str(numListings) + ' users'
 
-	output = {"type":"FeatureCollection","features":[], "polylines":[]}
+	output = {"type":"FeatureCollection","features":[]}
 
 	userDict = {}
 
@@ -115,29 +119,29 @@ def getData():
 			q.put('processing ' + str(i+1) + ' out of ' + str(numListings) + ' users...')
 
 			#SECOND QUERY TO DATABASE, GETTING CHECKINS FOR USERS MATCHING CNY CRITERIA
-			s = "SELECT expand(out_Checkin) FROM User WHERE uid = {}"
+			s = "SELECT expand(out_Checkin) FROM User WHERE uid = {} ORDER BY time ASC"
 
 			checkins = client.command(s.format(uid))
-
-			polyline = {"geometry":[]}
+			polyline = {"type":"Feature","properties":{},"geometry":{"type":"LineString", "coordinates":[]}}
 
 			numCheckins = len(checkins)
 			cids = [checkin.cid for checkin in checkins]
 			print 'user number ' + str(i+1) + ' has ' + str(numCheckins) + ' total checkins'
 
-			userDict[uid]['checkins'] = set(cids)
+			userDict[uid]['checkins'] = cids
+			polyline ["properties"]["user"] = uid
 
 			#THIRD AND FINAL QUERY TO DATABASE, FILTER CHECKINS
 			for cid in userDict[uid]['checkins']:
 
 				t = "SELECT lat, lng, time, cat_1 FROM CHECKIN WHERE cid = {} AND time BETWEEN '2014-01-21 00:01:00' AND '2014-02-13 00:00:00'"
-				#Note that query is not limited geographically: performance varied with number of users found.
+				#Note that query is not limited geographically: performance varies with limit on number of users.
 				#AND lat BETWEEN {} AND {} AND lng BETWEEN {} AND {}"
 
 				CNYcheckins = client.command(t.format(cid, lat1, lat2, lng1, lng2))
 				testBool = len(CNYcheckins)
 
-				print 'querying ' + str(cid) + ' for user ' + str(uid)
+				#print 'querying ' + str(cid) + ' for user ' + str(uid)
 
 				if len(CNYcheckins)!=0:
 
@@ -150,16 +154,17 @@ def getData():
 						q.put(str(j) + ' out of ' + str(numCheckins) + ' valid ...')
 
 						feature = {"type":"Feature","properties":{},"geometry":{"type":"Point"}}
-						feature ["user"] = uid
+						feature ["properties"]["user"] = uid
 						feature ["properties"]["time"] = daymaker(CNYcheckin.time)
-						feature ["properties"]["type"] = CNYcheckin.cat_1
-						feature ["geometry"]["coordinates"] = [CNYcheckin.lat, CNYcheckin.lng]
+						feature ["properties"]["type"] = cleanText(str(CNYcheckin.cat_1))
+						feature ["geometry"]["coordinates"] = [CNYcheckin.lng, CNYcheckin.lat]
 						print 'dayscore is ' + str(feature["properties"]["time"])
 
+						polyline["geometry"]["coordinates"].append([CNYcheckin.lng, CNYcheckin.lat])
+
 						output["features"].append(feature)
-						polyline["geometry"].append({'coordinates': [CNYcheckin.lat, CNYcheckin.lng]})
-						#print str(polylines)
-			output["polylines"].append(polyline)
+
+			output["features"].append(polyline)
 
 			q.put('idle')
 
--- static/script.js
+++ static/script.js
@@ -1,181 +1,180 @@
-//2016 JAVASCRIPT
+	//2016 JAVASCRIPT
 
-var eventOutputContainer = document.getElementById("message");
-var eventSrc = new EventSource("/eventSource");
+	var eventOutputContainer = document.getElementById("message");
+	var eventSrc = new EventSource("/eventSource");
 
-eventSrc.onmessage = function(e) {
-	console.log(e);
-	eventOutputContainer.innerHTML = e.data;
-};
+	eventSrc.onmessage = function(e) {
+		console.log(e);
+		eventOutputContainer.innerHTML = e.data;
+	};
 
-// DON'T FORGET TO CHANGE THIS CODE DEPENDING ON THE DATA YOU'RE DISPLAYING IN YOUR TOOLTIP
-var tooltip = d3.select("div.tooltip");
-var tooltip_category = d3.select("#cat");
+	// DON'T FORGET TO CHANGE THIS CODE DEPENDING ON THE DATA YOU'RE DISPLAYING IN YOUR TOOLTIP
+	var tooltip = d3.select("div.tooltip");
+	var tooltip_category = d3.select("#cat");
 
-var map = L.map('map').setView([22.799606, 113.567950], 9);
-
-
-		//this is the OpenStreetMap tile implementation
-		//L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    		//attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-		//}).addTo(map);
-		//uncomment for Mapbox implementation, and supply your own access token
-		L.tileLayer('https://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={accessToken}', {
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		mapid: 'mapbox.dark',
-		accessToken: "pk.eyJ1IjoiYWszNzkzIiwiYSI6ImNpZjdmZ3V5eTBpOXpzaGx6a3hvbjVoemQifQ.Fflvuzl9_moN4a8H_k4m0w"
-		}).addTo(map);
+	var map = L.map('map').setView([22.799606, 113.567950], 9);
 
 
-		//create variables to store a reference to svg and g elements
-		var svg_overlay = d3.select(map.getPanes().overlayPane).append("svg");
-		var g_overlay = svg_overlay.append("g").attr("class", "leaflet-zoom-hide");
+			//this is the OpenStreetMap tile implementation
+			//L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	    		//attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+			//}).addTo(map);
+			//uncomment for Mapbox implementation, and supply your own access token
+			L.tileLayer('https://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={accessToken}', {
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			mapid: 'mapbox.dark',
+			accessToken: "pk.eyJ1IjoiYWszNzkzIiwiYSI6ImNpZjdmZ3V5eTBpOXpzaGx6a3hvbjVoemQifQ.Fflvuzl9_moN4a8H_k4m0w"
+			}).addTo(map);
 
-		var svg = d3.select(map.getPanes().overlayPane).append("svg");
-		var g_line = svg.append("g").attr("class", "leaflet-zoom-hide");
-		var g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+			//create variables to store a reference to svg and g elements
+			var svg_overlay = d3.select(map.getPanes().overlayPane).append("svg");
+			var g_overlay = svg_overlay.append("g").attr("class", "leaflet-zoom-hide");
+
+			var svg = d3.select(map.getPanes().overlayPane).append("svg");
+			var g_line = svg.append("g").attr("class", "leaflet-zoom-hide");
+			var g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
 
-		function projectPoint(lat, lng) {
-			return map.latLngToLayerPoint(new L.LatLng(lat, lng));
-		}
-
-		function projectStream(lat, lng) {
-			var point = projectPoint(lat,lng);
-			this.stream.point(point.x, point.y);
-		}
-
-		var transform = d3.geo.transform({point: projectStream});
-		var path = d3.geo.path().projection(transform);
-
-		var toLine = d3.svg.line()
-			.interpolate("linear")
-			.x(function(d) {
-				return applyLatLngToLayer(d) .x
-			})
-			.y(function(d) {
-				return applyLatLngToLayer(d) .y
-			});
-
-		function applyLatLngToLayer(d) {
-			var y = d.geometry.coordinates[1]
-			var x = d.geometry.coordinates[0]
-			return map.latLngToLayerPoint(new L.LatLng(y, x))
-		}
-
-		var slidervalue = document.getElementById("slider").value;;
-
-		function sliderConvert(dayscore) {
-				dayscore = JSON.parse(dayscore)
-
-				if (dayscore == document.getElementById("slider").value){
-					return 1;}
-				else{
-				return .1;
-				}
+			function projectPoint(lat, lng) {
+				return map.latLngToLayerPoint(new L.LatLng(lat, lng));
 			}
 
-		function updateData(){
+			function projectStream(lat, lng) {
+				var point = projectPoint(lat,lng);
+				this.stream.point(point.x, point.y);
+			}
 
-			alert("Running");
+			var transform = d3.geo.transform({point: projectStream});
+			var path = d3.geo.path().projection(transform);
 
-			//var mapBounds = map.getBounds();
-			//var lat1 = mapBounds["_southWest"]["lat"];
-			//var lat2 = mapBounds["_northEast"]["lat"];
-			//var lng1 = mapBounds["_southWest"]["lng"];
-			//var lng2 = mapBounds["_northEast"]["lng"];
+			var toLine = d3.svg.line()
+				.interpolate("linear")
+				.x(function(d) {
+					return applyLatLngToLayer(d) .x
+				})
+				.y(function(d) {
+					return applyLatLngToLayer(d) .y
+				});
 
-			// CAPTURE USER INPUT FOR CELL SIZE FROM HTML ELEMENTS
-			//var cell_size = 25;
-			//var w = window.innerWidth;
-			//var h = window.innerHeight;
+			function applyLatLngToLayer(d) {
+				var y = d.geometry.coordinates[1]
+				var x = d.geometry.coordinates[0]
+				return map.latLngToLayerPoint(new L.LatLng(y, x))
+			}
 
-			// SEND USER CHOICES FOR ANALYSIS TYPE, CELL SIZE, HEAT MAP SPREAD, ETC. TO SERVER
-			request = "/getData"
-			//lat1=" + lat1 + "&lat2=" + lat2 + "&lng1=" + lng1 + "&lng2=" + lng2
-			//request2 = "/getData2?lat1=" + lat1 + "&lat2=" + lat2 + "&lng1=" + lng1 + "&lng2=" + lng2
+			var slidervalue = document.getElementById("slider").value;;
 
-			console.log(request);
+			function sliderConvert(dayscore) {
+					dayscore = JSON.parse(dayscore)
 
-			g.selectAll("circle").remove()
-			g_line.selectAll("line").remove()
+					if (dayscore == document.getElementById("slider").value){
+						return 1;}
+					else{
+					return .1;
+					}
+				}
 
-		  	d3.json(request, function(data) {
+			function updateData(){
 
-				//create placeholder circle geometry and bind it to data
-				var circles = g.selectAll("circle").data(data.features);
+				//var mapBounds = map.getBounds();
+				//var lat1 = mapBounds["_southWest"]["lat"];
+				//var lat2 = mapBounds["_northEast"]["lat"];
+				//var lng1 = mapBounds["_southWest"]["lng"];
+				//var lng2 = mapBounds["_northEast"]["lng"];
 
-					circles.enter()
-						.append("circle")
-						.on("mouseover", function(d){
-							tooltip.style("visibility", "visible");
-							tooltip_category.text("Category: " + d.properties.type);
-						})
-						.on("mousemove", function(){
-							tooltip.style("top", (d3.event.pageY-10)+"px")
-							tooltip.style("left",(d3.event.pageX+10)+"px");
-						})
-						.on("mouseout", function(){
-							tooltip.style("visibility", "hidden");
-						})
-						.attr("r",7)
-						.attr("fill-opacity", .1)
-						.style("fill", "white");
+				// CAPTURE USER INPUT FOR CELL SIZE FROM HTML ELEMENTS
+				//var cell_size = 25;
+				//var w = window.innerWidth;
+				//var h = window.innerHeight;
 
-				var lines = g_line.selectAll("line").data(data.polylines);
-						lines.enter().append("line")
+				// SEND USER CHOICES FOR ANALYSIS TYPE, CELL SIZE, HEAT MAP SPREAD, ETC. TO SERVER
+				request = "/getData"
+				//lat1=" + lat1 + "&lat2=" + lat2 + "&lng1=" + lng1 + "&lng2=" + lng2
+				//request2 = "/getData2?lat1=" + lat1 + "&lat2=" + lat2 + "&lng1=" + lng1 + "&lng2=" + lng2
 
-						update();
-						map.on("viewreset", update);
+				console.log(request);
 
-				function update() {
-					// get bounding box of data
-				    var bounds = path.bounds(data),
-				        topLeft = bounds[0],
-				        bottomRight = bounds[1];
-				    var buffer = 50;
-				    // reposition the SVG to cover the features.
-				    svg .attr("width", bottomRight[0] - topLeft[0] + (buffer * 2))
-				        .attr("height", bottomRight[1] - topLeft[1] + (buffer * 2))
-				        .style("left", (topLeft[0] - buffer) + "px")
-				        .style("top", (topLeft[1] - buffer) + "px");
+				g.selectAll("circle").remove()
+				g_line.selectAll("path").remove()
 
-				    g   .attr("transform", "translate(" + (-topLeft[0] + buffer) + "," + (-topLeft[1] + buffer) + ")");
-						g_line.attr("transform", "translate(" + (-topLeft[0] + buffer) + "," + (-topLeft[1] + buffer) + ")");
+			  	d3.json(request, function(data) {
 
-				    // update circle position and size
-				    circles
-				    	.attr("cx", function(d) { return projectPoint(d.geometry.coordinates[0], d.geometry.coordinates[1]).x; })
-				    	.attr("cy", function(d) { return projectPoint(d.geometry.coordinates	[0], d.geometry.coordinates[1]).y; })
-							;
+					//create placeholder circle geometry and bind it to data
+					var circles = g.selectAll("circle").data(data.features);
+
+						circles.enter()
+							.append("circle")
+							.on("mouseover", function(d){
+								tooltip.style("visibility", "visible");
+								tooltip_category.text("Category: " + d.properties.type);
+							})
+							.on("mousemove", function(){
+								tooltip.style("top", (d3.event.pageY-10)+"px")
+								tooltip.style("left",(d3.event.pageX+10)+"px");
+							})
+							.on("mouseout", function(){
+								tooltip.style("visibility", "hidden");
+							})
+							.attr("r",7)
+							.attr("fill-opacity", .1)
+							.style("fill", "white");
+
+					var lines = g_line.selectAll(".lineConnect").data(data.polylines);
+							lines.enter().append("path")
+							.attr("class", "lineConnect")
+
+							update();
+							map.on("viewreset", update);
+
+					function update() {
+						// get bounding box of data
+					    var bounds = path.bounds(data),
+					        topLeft = bounds[0],
+					        bottomRight = bounds[1];
+					    var buffer = 50;
+					    // reposition the SVG to cover the features.
+					    svg .attr("width", bottomRight[0] - topLeft[0] + (buffer * 2))
+					        .attr("height", bottomRight[1] - topLeft[1] + (buffer * 2))
+					        .style("left", (topLeft[0] - buffer) + "px")
+					        .style("top", (topLeft[1] - buffer) + "px");
+
+					    g   .attr("transform", "translate(" + (-topLeft[0] + buffer) + "," + (-topLeft[1] + buffer) + ")");
+							g_line.attr("transform", "translate(" + (-topLeft[0] + buffer) + "," + (-topLeft[1] + buffer) + ")");
+
+					    // update circle position and size
+					    circles
+					    	.attr("cx", function(d) { return projectPoint(d.geometry.coordinates[1], d.geometry.coordinates[0]).x; })
+					    	.attr("cy", function(d) { return projectPoint(d.geometry.coordinates[1], d.geometry.coordinates[0]).y; })
+								;
 
 							lines
 								.attr("d", toLine)
-				};
+					};
+					console.log(lines)
 
 
-
-					update()
-					changeSlider();})
-
-
-				}
-
-				function changeSlider(){
-					console.log(document.getElementById("slider").value)
-
-					g.selectAll("circle")
-						.attr("r",7)
-						.attr("fill-opacity", .1)
-						.style("fill", "white")
-						.filter( function (d) {return(d.properties.time == document.getElementById("slider").value)})
-							.attr("fill-opacity", 1)
-							.style("fill", "#FFB218")
-							.attr("r", 10)
-				};
-// call function to update geometry
+						update()
+						changeSlider();})
 
 
+					}
+
+					function changeSlider(){
+						console.log(document.getElementById("slider").value)
+
+						g.selectAll("circle")
+							.attr("r",7)
+							.attr("fill-opacity", .1)
+							.style("fill", "white")
+							.filter( function (d) {return(d.properties.time == document.getElementById("slider").value)})
+								.attr("fill-opacity", 1)
+								.style("fill", "#FFB218")
+								.attr("r", 10)
+					};
+	// call function to update geometry
+
+
+		;
 	;
-;
-		updateData();
+			updateData();
